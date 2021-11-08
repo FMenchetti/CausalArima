744e54d09ad28e2e03f7a026a89f9764905eb287
Fiammetta Menchetti <fiammetta.menchetti@gmail.com>
2021-11-08 15:57:43 +0100

    add pvalue column in impact table

--- R/table_arima.R
+++ R/table_arima.R
@@ -78,10 +78,11 @@ impact <- function(x, format="numeric", nboot=10, horizon=NULL, ...){
   # 1.2. coefficient estimates
   coef<-x$model$coef
   se<-sqrt(diag(x$model$var.coef))
+  pvalue <- coef/se
   if(length(coef)!=0){
-  param<-data.frame(coef, se)
+  param<-data.frame(coef, se, pvalue)
   rownames(param)<- names(x$model$coef)
-  colnames(param)<-c("coef", "se")
+  colnames(param)<-c("coef", "se", "pvalue")
   }
   else{ param<-NULL}
 
